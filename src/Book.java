41c1658444a270ef25e536f7afcfcce2a21ad081
deanogeorge13 <deanogeorge13@hotmail.com>
2018-10-03 16:40:20 +0100

    lab3

---
+++ src/Book.java
@@ -0,0 +1,54 @@
+public class Book {
+    private String title;
+    private double price;
+    private String ISBN;
+    private int noOfPages;
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public int getNoOfPages(){
+        return noOfPages;
+    }
+
+    public void setNoOfPages(int noOfPages){
+        this.noOfPages = noOfPages;
+    }
+
+    public String getISBN(){
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public String toString(){
+        return "Title: " + title + "\nPrice: " + price + ....;
+    }
+
+    public Book{
+        title = "No Title";
+        price = 0.00;
+        ISBN = "No ISBN";
+        noOfPages = 0;
+    }
+
+    public Book(String title, double price, String ISBN, int noOfPages){
+        //setTitle();
+    }
+
+}
---
+++ src/Book1.java
@@ -0,0 +1,5 @@
+public class Book1 {
+    public static void main(String[] args) {
+
+    }
+}
