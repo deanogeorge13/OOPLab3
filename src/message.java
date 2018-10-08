4b4bd28250272438dea39e9da5735e9776f7fd9e
deanogeorge13 <deanogeorge13@hotmail.com>
2018-10-08 13:54:15 +0100

    srdiklo;p

---
+++ src/message.java
@@ -0,0 +1,27 @@
+public class message {
+    private String recipient;
+    private String sender;
+    private String text;
+
+    public message(String recipient, String sender, String text)
+    {
+        setRecipient(recipient);
+        setSender(sender);
+        setText(text);
+    }
+
+    private void setRecipient(String recipient)
+    {
+        this.recipient = recipient;
+    }
+
+    private void setSender(String sender)
+    {
+        this.sender = sender;
+    }
+
+    private void setText(String text)
+    {
+        this.text = text;
+    }
+}
